5bf7624001a0336c393fedea4d5613f2882de3c8
youxiu326 <youxiu326@163.com>
2019-06-02 12:13:33 +0800

    aaaaaa

--- src/main/java/com/youxiu326/controller/LoginCtrl.java
+++ src/main/java/com/youxiu326/controller/LoginCtrl.java
@@ -4,7 +4,9 @@ import com.youxiu326.common.JsonResult;
 import com.youxiu326.entity.Account;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -39,7 +41,7 @@ public class LoginCtrl {
         return result.success("登陆成功");
     }
 
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     @ResponseBody
     public JsonResult logout(HttpServletRequest req,Account account){
 
