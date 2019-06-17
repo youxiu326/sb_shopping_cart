cd9635648f72488ac2e0a7489806dc9be109ff69
youxiu3266 <2473538988@qq.com>
2019-06-17 18:04:23 +0800

    添加注释

--- src/main/java/com/youxiu326/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/youxiu326/service/impl/ShoppingCartServiceImpl.java
@@ -22,6 +22,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    /**
+     * 获得用户key
+     *
+     * 1.用户未登录情况下第一次进入购物车  -> 生成key 保存至cookie中
+     * 2.用户未登录情况下第n进入购物车    -> 从cookie中取出key
+     * 3.用户登录情况下                  -> 根据用户code生成key
+     * 4.用户登录情况下并且cookie中存在key-> 从cookie取的的key从缓存取得购物车 合并至
+     *  用户code生成key的购物车中去 ，这样后面才能根据用户code 取得正确的购物车
+     *
+     * @param req
+     * @param resp
+     * @param account
+     * @return
+     */
     @Override
     public String getKey(HttpServletRequest req, HttpServletResponse resp, Account account) {
         //https://github.com/youxiu326/sb_shiro_session.git
